31b46f2d5c3ec250b55219abd452a31d4fc6746f
Amina Maqsood <amnaamaqsood61@gmail.com>
2026-08-02 00:02:26 +0500

    Add TypeScript types to request and response serializers

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -6,18 +6,19 @@ import { logger } from "./lib/logger";
 
 const app: Express = express();
 
+// @ts-ignore
 app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
@@ -25,10 +26,10 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/api", router);
 
 export default app;
